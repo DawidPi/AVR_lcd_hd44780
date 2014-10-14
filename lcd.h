02e4fcd7cf8eda6401a74b97eb08e9ac05dfb1eb
Dawid Pilarski <pilarski.dawid94@gmail.com>
2014-10-15 01:52:05 +0200

    License info deleted

--- lcd.h
+++ lcd.h
@@ -11,7 +11,7 @@
 /* Biblioteka działa tylko z wykorzystanym RW i dla wyświetlaczy zgodnych ze standardem HD44780
  * o rozmiarze 2x16. Inne wyświetlacze nie są obsługiwane przez bibliotekę. Wszystkie wyprowadzenia
  * muszą być podłączone do tego samego portu wyprowadzeń. Obsługuje jedynie pamięć RAM. Brak obsługi np. EEPROM.
- * Wydany na licencji BSD-new. Autor: Dawid Pilarski. Maksymalnie wyświetla 5cyfrową liczbę + znak
+ * Autor: Dawid Pilarski. Maksymalnie wyświetla 5cyfrową liczbę + znak
  */
 
 //commands
@@ -106,32 +106,5 @@ void lcd_set_pos2(position const *);			//as higher but uses ptr for structure po
 void lcd_port_disp(char port_name);	//where argument can be 'A' 'B' 'C' 'D'
 void lcd_pin_disp(char pin_name);	//not tested!!
 
-/* treść licencji:
- * Copyright (c) 2014, Dawid Pilarski
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of the <organization> nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
 
 #endif /* LCD_H_ */
